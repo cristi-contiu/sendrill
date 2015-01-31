0949f578c1b08cf8c06afa516cbefe1de1d59812
Cristi Contiu <cristi@contiu.ro>
2015-01-31 13:31:50 +0200

    default config file

--- app/config.php
+++ app/config.php
@@ -8,8 +8,8 @@
  *  Sendy-Webhook settings
  */
 define('SW_SENDY_CONFIG_FILE',     '/../includes/config.php');
-define('SW_MANDRILL_WEBHOOK_URL',  'http://email.bixel.ro/smtp-webhooks/mandrill');   // the webhook URL EXACTLY as entered in Mandrill (including query parameters)
-define('SW_MANDRILL_WEBHOOK_KEY',  'FSmbtWP6xKk4o1FsBL1gPg');                    // the webhook key given by Mandrill
-define('SW_ALLOWED_SOFT_BOUNCES',  3);                                           // number of allowed soft bounces before converting to hard bounce
-define('SW_DEBUG',                 true);
+define('SW_MANDRILL_WEBHOOK_URL',  '');   // the webhook URL EXACTLY as entered in Mandrill (including query parameters)
+define('SW_MANDRILL_WEBHOOK_KEY',  '');   // the webhook key given by Mandrill
+define('SW_ALLOWED_SOFT_BOUNCES',  3);    // number of allowed soft bounces before converting to hard bounce
+define('SW_DEBUG',                 false);
 define('SW_LOG_FILE',              'app/app.log');
