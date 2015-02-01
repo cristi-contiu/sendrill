345385c78ae2d817309f3bf7a2170652430e23cb
Cristi Contiu <cristi@contiu.ro>
2015-02-01 11:24:22 +0200

    main application request handler

---
+++ src/App.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Main application class
+ *
+ * @package    CristiContiu\SendySMTPWebhooks
+ * @author     Cristi Contiu <cristi@contiu.ro>
+ * @license    MIT
+ * @link       https://github.com/cristi-contiu/sendy-smtp-webhooks
+ */
+
+namespace CristiContiu\SendySMTPWebhooks;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use CristiContiu\SendySMTPWebhooks\Providers;
+use CristiContiu\SendySMTPWebhooks\SendyListener;
+
+class App
+{
+    /**
+     * Handles the HTTP Request and returns a response
+     *
+     * @param  Request   $request 
+     * @param  array     $config 
+     * @return Response 
+     */
+    static public function handle( Request $request, $config )
+    {
+        $appLog = new Logger( get_class() );
+        $appLog->pushHandler($config['logHandler']);
+
+        // checking and creating requested povider
+        $getProvider = $request->query->get('provider');
+        if ( isset($config['providers'][$getProvider]) ) {
+            $provider = new $config['providers'][$getProvider]['class']($request, $config);
+        } else {
+            $appLog->addError("Unsupported provider '$getProvider'", (array) $request);
+            return new Response('Unsupported provider', Response::HTTP_NOT_IMPLEMENTED);
+        }
+
+        if ( $provider->authenticate() == false ) {
+            $appLog->addError('Unauthorized', (array) $request);
+            return new Response('Unauthorized', Response::HTTP_UNAUTHORIZED);
+        }
+
+        if ( $provider->readEvents() == false ) {
+            $appLog->addError('Invalid input', (array) $request);
+            return new Response('Invalid input', Response::HTTP_BAD_REQUEST);
+        }
+
+        try {
+            $sendyListener = new SendyListener($config);
+        } catch (\Exception $exc) {
+            return new Response($exc->getMessage(), $exc->getCode());
+        }
+
+        $provider->processEvents( $sendyListener );
+        return new Response('OK', Response::HTTP_OK);
+    }
+
+    /**
+     * Reads default and Sendy configs and sets app-wide logger handler
+     *
+     * @return array 
+     */
+    static public function getConfigFromFiles()
+    {
+        $config = require __DIR__ . '/../app/default.config.php';
+        $config['sendy'] = self::getSendyConfig($config['sendyConfigFile']);
+        $config['logHandler'] = new StreamHandler(__DIR__ . '/../' . $config['logFile'], $config['debug'] ? Logger::DEBUG : Logger::INFO);
+        return $config;
+    }
+
+    /**
+     * Returns variables defined in Sendy config as array
+     *
+     * @param  string $sendyConfigFile 
+     * @return array 
+     */
+    static public function getSendyConfig( $sendyConfigFile )
+    {
+        require __DIR__ . '/../' . $sendyConfigFile; 
+        unset($sendyConfigFile);
+        return get_defined_vars();
+    }
+}
