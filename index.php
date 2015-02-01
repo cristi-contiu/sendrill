21b27c918126d29ff6fc415e656405af3305a14e
Cristi Contiu <cristi@contiu.ro>
2015-02-01 11:20:53 +0200

    slim front controller

--- index.php
+++ index.php
@@ -1,43 +1,20 @@
 <?php
-
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
-use CristiContiu\SendySMTPWebhooks\Tracker;
-use CristiContiu\SendySMTPWebhooks\Providers\ProviderFactory;
+/**
+ * Front controller for Sendy SMTP Webhooks
+ *
+ * @package    CristiContiu\SendySMTPWebhooks
+ * @author     Cristi Contiu <cristi@contiu.ro>
+ * @license    MIT
+ * @link       https://github.com/cristi-contiu/sendy-smtp-webhooks
+ */
 
 require_once(__DIR__ . '/vendor/autoload.php');
-require_once(__DIR__ . '/app/config.php');
-require_once(__DIR__ . SW_SENDY_CONFIG_FILE);
 
-$globalLogHandler = new StreamHandler(__DIR__ . '/' . SW_LOG_FILE, SW_DEBUG ? Logger::DEBUG : Logger::INFO);
-$logger = new Logger('App');
-$logger->pushHandler($globalLogHandler);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use CristiContiu\SendySMTPWebhooks\App;
 
-try {
-    $requestedProvider = rtrim($_GET['provider'], "/");
-    $provider = ProviderFactory::createByName( $requestedProvider , $globalLogHandler );
-    if ( $provider == false ) {
-        $logger->addError('Unsupported provider', $_GET);
-        throw new Exception('Unsupported provider', 500);
-    }
-    
-    $provider->setParamsFromGlobals();
-    
-    if ( $provider->authenticate() == false ) {
-        $logger->addError('Unauthorized', $provider->getParams()->toArray());
-        throw new Exception('Unauthorized', 401);
-    }
-
-    if ( $provider->readEvents() == false ) {
-        $logger->addError('Invalid input', $provider->getParams()->toArray());
-        throw new Exception('Invalid input', 400);
-    }
-
-    $tracker = new Tracker( $globalLogHandler, $dbHost, $dbUser, $dbPass, $dbName, $dbPort, $charset );
-    $provider->processEvents( $tracker );
-} catch (Exception $exc) {
-    if ( !headers_sent() ) {
-        header( $_SERVER['SERVER_PROTOCOL'] . ' ' . $exc->getCode() . ' ' . $exc->getMessage() );
-    }
-    die( $exc->getCode() . ' ' . $exc->getMessage() );
-}
+$request = Request::createFromGlobals();
+$config = App::getConfigFromFiles();
+$response = App::handle($request, $config);
+$response->send();
